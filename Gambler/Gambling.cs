d100b6c612e805c3f4edab941e0fdf59daece958
janhavi07 <56464138+janhavi07@users.noreply.github.com>
2021-04-12 12:41:38 +0530

    [Janhavi]Add Gambler program

---
+++ Gambler/Gambling.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gambler
+{
+    class Gambling
+    {
+        Random random = new Random();
+        private readonly int WIN = 1;
+        private readonly int BET = 1;
+
+        public int RandomNumberGenerator()
+        {           
+                return random.Next(0, 2);                                
+        }
+        public void GamblingLogic(int stake, int goal)
+        {
+            int winningCount = 0, LosingCount = 0;
+            while (stake < goal && stake > 0)
+            {
+                int winLose = RandomNumberGenerator();
+                if (winLose == WIN)
+                {
+                    stake += BET;
+                    winningCount++;
+                }
+                else
+                {
+                    stake -= BET;
+                    LosingCount++;
+                }
+            }
+            double percentageWin = (winningCount*100 / (winningCount+LosingCount));
+            double percentageLose = (LosingCount*100 / (winningCount+LosingCount));
+            Console.WriteLine("Winning percent  is " + percentageWin);
+            Console.WriteLine("Losing percent  is " + percentageLose);
+         }
+    }
+}
---
+++ Gambler/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Gambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gambling gambler = new Gambling();
+
+            Console.WriteLine("Wlecome to the Gambler game!");
+            Console.WriteLine("Enter No of Stakes:");
+            int stake = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Goal:");
+            int goal = Convert.ToInt32(Console.ReadLine());
+            gambler.GamblingLogic(stake,goal);
+
+        }
+    }
+}
